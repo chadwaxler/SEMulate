62367484aa5c89a00d4ae3a0e30a24a4e176b048
Chad Waxler <chadwaxler@gmail.com>
2024-12-02 10:07:17 -0600

    new build

---
+++ docs/chunk-A35OR74Y.js
@@ -0,0 +1,1 @@
+import{Da as o,Pa as n,Qa as i,Va as a}from"./chunk-YA72N5MZ.js";var c=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["sem-about"]],decls:2,vars:0,template:function(t,p){t&1&&(n(0,"p"),a(1,"about works!"),i())},encapsulation:2})}return e})();export{c as AboutComponent};
